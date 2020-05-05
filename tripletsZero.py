5556e5d7a27ba4a8fab53e5b9feec2d28581ada4
Mirav Gokani <mirav.gokani@gmail.com>
2020-05-05 17:55:00 -0400

    Update tripletsZero.py

--- tripletsZero.py
+++ tripletsZero.py
@@ -1,21 +1,44 @@
-"""
-In an array, find all triplets with sum 0
-"""
+'''
+Given an array of distinct elements. The task is to find triplets in the array whose sum is zero.
 
-class Solution(object):
-  def tripletsZero(self, A):
-    """
-    :type A: list
-    :rtype: list
-    """
-    l =  len(A)
-    if l < 3: 
-      print(None)
-    for i in range(2, l):
-      if A[i] + A[i-1] + A[i-2] == 0:
-        print(A[i], A[i-1], A[i-2])
+Examples :
+
+Input : arr[] = {0, -1, 2, -3, 1}
+Output : (0 -1 1), (2 -3 1)
+
+Explanation : The triplets with zero sum are
+0 + -1 + 1 = 0 and 2 + -3 + 1 = 0  
+
+Input : arr[] = {1, -2, 1, 0, 5}
+Output : 1 -2  1
+Explanation : The triplets with zero sum is
+1 + -2 + 1 = 0   
+'''
+
+'''
+brute force way is to run 3 loops - i -> 0 to n-2, j -> i+1 to n-1 and k -> j+1 to n
+
+more efficient way is to run 2 loops - i -> 0 to n-1, j -> i+1 to n and check if -(arr[i] + arr[j]) is present in the set. If yes, print/return the pair, else add the element to the set
+'''
+
+
+def findTriplets(arr): 
+    n = len(arr)
+    found = False
+    for i in range(n - 1): 
+    # set must be created after the first loop to avoid duplicate entries
+        s = set() 
+        for j in range(i + 1, n): 
+            x = -(arr[i] + arr[j]) 
+            if x in s: 
+                print(x, arr[i], arr[j]) 
+                found = True
+            else: 
+                s.add(arr[j])
+        print(s) 
+    if found == False: 
+        print("No Triplet Found") 
 
 if __name__ == "__main__":
-  A = [0, -2, 2]
-  o = Solution()
-  ret = o.tripletsZero(A)
+  a = [0, -1, 2, -3, 1]
+  findTriplets(a)
